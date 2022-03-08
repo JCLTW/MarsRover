825935fdf9bc5a96ee1f242c9d8b7905d7d84176
Jiancheng LI <jiancheng.li@thoughtworks.com>
2022-03-08 22:00:13 +0800

    Step 0, Init Project

---
+++ MarsRoverTest/RoverTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace MarsRoverTest
+{
+    public class RoverTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
